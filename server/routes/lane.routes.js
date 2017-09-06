85b8ec273e96f1a27665d9deb17dedfdbf19ca5a
Bogna Konstanty <rosiakb@gmai.com>
2017-09-06 12:52:27 +0200

    add files

---
+++ server/routes/lane.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import * as LaneController from '../controllers/lane.controller';
+import noteRouter from './note.routes';
+
+const router = new Router();
+
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+router.use('/lanes/:laneId/', noteRouter);
+
+export default router;
